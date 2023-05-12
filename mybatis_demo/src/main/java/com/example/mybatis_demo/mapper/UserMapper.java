00539fcd192850b2d15aedd84918bb9b9c9f67c7
Dawn <2096177261@qq.com>
2023-05-12 09:50:21 +0800

    add

--- mybatis_demo/src/main/java/com/example/mybatis_demo/mapper/UserMapper.java
+++ mybatis_demo/src/main/java/com/example/mybatis_demo/mapper/UserMapper.java
@@ -6,6 +6,7 @@ import org.apache.ibatis.annotations.Param;
 
 @Mapper
 public interface UserMapper {
-
+    // 根据用户id 查询用户
     public UserInfo getUserById(@Param("id") Integer id); // @Param("id"): SQL中的id, Integer id: 参数id
+
 }
