3e18c5dbbdf74c81cd5be383a399d3ad73535629
SGJ <2096177261@qq.com>
2023-05-22 07:32:30 +0800

    add

---
+++ seleniumDemo/src/test/java/com/webautotest/RunAutoTest.java
@@ -0,0 +1,12 @@
+package com.webautotest;
+
+/**
+ * @author sgj
+ * @create 2023-05-20 9:01
+ */
+public class RunAutoTest {
+    public static void main(String[] args) throws InterruptedException {
+        FirstAutoTest firstAutoTest = new FirstAutoTest();
+        firstAutoTest.nameTest();
+    }
+}
