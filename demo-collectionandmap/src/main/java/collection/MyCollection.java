c2021c3127190416d618442f24addf573c1c5bbd
SGJ <2096177261@qq.com>
2023-06-06 20:41:37 +0800

    add

--- demo-collectionandmap/src/main/java/collection/MyCollection.java
+++ demo-collectionandmap/src/main/java/collection/MyCollection.java
@@ -1,8 +1,13 @@
 package collection;
 
+import java.util.Collection;
+
 /**
  * @author sgj
  * @create 2023-06-06 20:36
  */
 public class MyCollection {
+    public static void main(String[] args) {
+        Collection
+    }
 }
